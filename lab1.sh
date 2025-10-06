ec8a361ab0afa869847b48b41b45861a0dc4fd77
burntkayatoast <reneelow0205@gmail.com>
2025-10-06 17:14:33 +0100

    vagrant lab3 pushed

--- lab1.sh
+++ lab1.sh
@@ -1,31 +1,10 @@
-#! usr/bin/bash
+#! /usr/bin/env bash
 
-sudo apt update && sudo apt ugrade
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y nano vim python-is-python3 python3-venv python3-pip
 
-sudo apt install nano vim python-is-python3
-sudo apt install python3-venv python3-pip
-
-python -m venv .my_venv
-source .my_venv/bin/activate
+python -m venv /home/vagrant/.my_venv
+source /home/vagrant/.my_venv/bin/activate
 
 pip install flask
-
-cat > hello.py << 'EOF'
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def say_hello():
-	return '''<p>Hello, World, I am a Flask app!</p>'''
-
-@app.route('/about')
-def link_doc():
-	return '''<p>This application is running on the Flask web framework.
-		<br><br>
-		<a href="https://flask.palletsprojects.com/en/stable">Link to documentation.</a>
-		</p>'''
-
-EOF
-
-flask --app hello run --host=0.0.0.0
+flask --app /vagrant/hello.py run --host=0.0.0.0
